df73691a6bf3c361bcff5ebaf5e2ae825e2a6a83
siddharthsiva <siddharthsivalanka@gmail.com>
2025-10-24 16:58:20 -0700

    test

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,9 +1,13 @@
 from fastapi import FastAPI
-from app.db.database import Base, engine
-from app.routers import users, requests
+from .db.database import Base, engine
+from .routers import users, requests
 
+# Create database tables
 Base.metadata.create_all(bind=engine)
 
+# Initialize FastAPI app
 app = FastAPI()
+
+# Include routers
 app.include_router(users.router)
 app.include_router(requests.router)
